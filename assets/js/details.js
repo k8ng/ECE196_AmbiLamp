66f2470712128d8c63fc8eb5e832af5e5c21da8e
Tony Wu <ttwu@ucsd.edu>
2018-10-18 11:33:06 -0700

    finished details charts prototype

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,22 +1,33 @@
 setActivePage('nav_details');
+document.getElementById('first-chart').click();
 
-function showLineChart(elem) {
+function showLineChart(elem, data) {
+  var oldCtx = document.querySelector('canvas');
+  var parent = oldCtx.parentNode;
+  parent.removeChild(oldCtx);
+  var ctx = document.createElement('canvas');
+
+  parent.appendChild(ctx);
+
   var items = elem.parentNode.children;
   for (i = 0; i < items.length; i++) {
     items[i].classList.remove('active');
   }
   elem.classList.add('active');
+
+  renderChart(ctx, data);
 }
 
-var data = {
-    datasets: [{
-      data: [10,30,40,20,10]
-    }],
-    labels: ['1','2','3','4','5']
-}
+function renderChart(ctx,data) {
+  var data = {
+      datasets: [{
+        data: data
+      }],
+      labels: ['1','2','3','4','5']
+  }
 
-var ctx = document.querySelector('canvas');
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-});
+  var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: data
+  });
+}
