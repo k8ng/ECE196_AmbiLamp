b82b28715a6ea2f5f5f573edba7ba31f7e7e786d
Kyle Ng <k8ng@ucsd.edu>
2018-10-23 12:02:20 -0700

    prevent divide by 0 for timeTotal

--- helpers/stats.js
+++ helpers/stats.js
@@ -31,7 +31,7 @@ exports.resetStats = function(req, res) {
 	    'timeInDry': 0,
 	    'timeInHumid': 0,
 	    'timeOn': 0,
-	    'timeTotal': 0
+	    'timeTotal': 1
 	}
 
 	db.Stats.findOneAndUpdate({}, blank, {'new': true, upsert: true})
